e09804b468418114d34808815fba534902def229
sundayonah <sundayonah94@gmail.com>
2023-03-01 10:24:41 +0100

    back and front end

---
+++ frontEnd/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h>hello Gee</h>
+      </header>
+    </div>
+  );
+}
+
+export default App;
